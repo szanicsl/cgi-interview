088f0308b99839608068a76d066715757d1a8234
szanicsl <szanicsl@gmail.com>
2022-06-01 22:22:28 +0200

    Removing boilerplate code, setting PeopleSetup as a utility class

--- src/main/java/com/cgi/boat/interview/PeopleSetup.java
+++ src/main/java/com/cgi/boat/interview/PeopleSetup.java
@@ -5,6 +5,11 @@ import java.util.Collections;
 import java.util.List;
 
 class PeopleSetup {
+
+    private PeopleSetup(){
+        throw new java.lang.UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
     final static List<Person> people = Collections.unmodifiableList(Arrays.asList(
             new Person("Antonio","Aakeel"),
             new Person("Khalid","Abdalla"),
--- src/main/java/com/cgi/boat/interview/Person.java
+++ src/main/java/com/cgi/boat/interview/Person.java
@@ -1,20 +1,9 @@
 package com.cgi.boat.interview;
 
-class Person {
+import lombok.Data;
 
+@Data
+class Person {
     private final String firstName;
     private final String lastName;
-
-    Person(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    String getFirstName() {
-        return firstName;
-    }
-
-    String getLastName() {
-        return lastName;
-    }
 }
