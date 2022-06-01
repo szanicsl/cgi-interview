f10c8b50fdda4abf2c04a536012ca8a413a50bfa
szanicsl <szanicsl@gmail.com>
2022-06-01 22:18:01 +0200

    Add test cases

--- src/test/java/com/cgi/boat/interview/PeopleProcessorTest.java
+++ src/test/java/com/cgi/boat/interview/PeopleProcessorTest.java
@@ -1,4 +1,44 @@
 package com.cgi.boat.interview;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 public class PeopleProcessorTest {
+
+    static List<Person> people;
+
+    @BeforeAll
+    static void setup(){
+        people = Collections.unmodifiableList(Arrays.asList(
+                new Person("John","Doe"),
+                new Person("John","Silver"),
+                new Person("Peter","Doe")
+        ));
+    }
+
+    @Test
+    void testLastnamesByFirstname(){
+        Map<String, List<String>> expected = Collections.unmodifiableMap(Map.of(
+                "John", Arrays.asList("Doe", "Silver"),
+                "Peter", Arrays.asList("Doe")
+        ));
+
+        Assertions.assertEquals(expected, PeopleProcessor.lastnamesByFirstname(people));
+    }
+
+    @Test
+    void testFirstnamesByLastname(){
+        Map<String, List<String>> expected = Collections.unmodifiableMap(Map.of(
+                "Doe", Arrays.asList("John", "Peter"),
+                "Silver", Arrays.asList("John")
+        ));
+
+        Assertions.assertEquals(expected, PeopleProcessor.firstnamesByLastname(people));
+    }
 }
